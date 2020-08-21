19c41a460f07e836dfc784d804d25c8ee3e29483
antiproton85 <kevinmcgr@gmail.com>
2020-08-21 09:46:08 -0400

    Update api

--- sfdc/api.py
+++ sfdc/api.py
@@ -8,3 +8,18 @@ def report_status(ssentry_id: str, status: str):
     }
 
     SalesforceClient.rest_apex(rest_path=rest_path, method='POST', payload=payload)
+
+
+def report_error(ssentry_id: str, err_msg: str):
+    rest_path = '/symphony/sse-status'
+    payload = {
+        "ssentry_id": ssentry_id,
+        "status": "error",
+        "message": err_msg
+    }
+
+    SalesforceClient.rest_apex(rest_path=rest_path, method='POST', payload=payload)
+
+
+def report_system_error(err_msg: str):
+    pass
