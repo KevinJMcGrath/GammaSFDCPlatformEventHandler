94e6351b4738999b90299fb725e9bc2ee1ddc71c
antiproton85 <kevinmcgr@gmail.com>
2020-08-31 15:30:39 -0400

    Add docker-compose command

--- app_deploy.ps1
+++ app_deploy.ps1
@@ -1,3 +1,8 @@
-docker build . -t kevinmcgrath/gamma-tenant-manager:latest
+docker build . -t kevinmcgrath/gamma-tenant-manager:latest -t kevinmcgrath/gamma-tenant-manager:0.1.3
 
 docker push kevinmcgrath/gamma-tenant-manager:latest
+docker push kevinmcgrath/gamma-tenant-manager:0.1.3
+
+# On the VM:
+# docker-compose up -d --build gamma-tenant
+ #>
