b62541f255f2dfd1a08a35162dfadd0dc09ef40e
antiproton85 <kevinmcgr@gmail.com>
2020-09-10 09:18:51 -0400

    Add refresh logic to the SFClient - simple-salesforce doesn't handle token refresh properly

--- app_deploy.ps1
+++ app_deploy.ps1
@@ -1,7 +1,12 @@
-docker build . -t kevinmcgrath/gamma-tenant-manager:latest -t kevinmcgrath/gamma-tenant-manager:0.1.3
+# Trying to build with both tags at once. Didn't work as I expected it to.
+# docker build . -t kevinmcgrath/gamma-tenant-manager:latest -t kevinmcgrath/gamma-tenant-manager:0.1.5
+
+docker build . -t kevinmcgrath/gamma-tenant-manager:0.1.5
+
+docker tag kevinmcgrath/gamma-tenant-manager:0.1.5 kevinmcgrath/gamma-tenant-manager:latest
 
 docker push kevinmcgrath/gamma-tenant-manager:latest
-docker push kevinmcgrath/gamma-tenant-manager:0.1.3
+docker push kevinmcgrath/gamma-tenant-manager:0.1.5
 
 # On the VM:
 # docker-compose up -d --build gamma-tenant
--- sfdc/apex_client.py
+++ sfdc/apex_client.py
@@ -1,26 +1,43 @@
 import logging
 
 from simple_salesforce import Salesforce
+from simple_salesforce.exceptions import SalesforceExpiredSession
 
 
 class SFClient:
     def __init__(self, sym_config):
+        self.sym_config = sym_config
         self.domain = 'test' if sym_config['sandbox'] else 'login'
-        self.client = self.init_client(sym_config)
+        self.client = self.init_client()
 
-    def init_client(self, sym_config):
+    def init_client(self):
         logging.debug('Initializing Salesforce API client...')
-        c = Salesforce(username=sym_config['username'], password=sym_config['password'],
-                          security_token=sym_config['security_token'], domain=self.domain)
+        c = Salesforce(username=self.sym_config['username'], password=self.sym_config['password'],
+                          security_token=self.sym_config['security_token'], domain=self.domain, client_id='Gamma M.S.')
 
-        api_key = sym_config['custom_api_key']
+        api_key = self.sym_config['custom_api_key']
         c.headers['X-SYM-APIKEY'] = api_key
 
         return c
 
-
     def rest_apex(self, rest_path: str, method: str='GET', payload=None):
+        retry: bool = False
+
         try:
             self.client.apexecute(action=rest_path, method=method, data=payload)
+        except SalesforceExpiredSession as sfdc_ex:
+            retry = True
+            logging.error(sfdc_ex)
+
         except Exception as ex:
             logging.error(ex)
+
+        if retry:
+            try:
+                logging.info('Attempting to reinitialize the Client and resending the query...')
+                self.client = self.init_client()
+
+                self.client.apexecute(action=rest_path, method=method, data=payload)
+
+            except Exception as ex:
+                logging.error(ex)
