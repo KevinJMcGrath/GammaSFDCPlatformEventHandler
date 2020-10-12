0e972fb1a70ada01602d202f4f9c378201e34544
antiproton85 <kevinmcgr@gmail.com>
2020-10-12 11:33:47 -0400

    Add config option to enable/disable finalizing tenants

--- app_deploy.ps1
+++ app_deploy.ps1
@@ -1,12 +1,12 @@
 # Trying to build with both tags at once. Didn't work as I expected it to.
 # docker build . -t kevinmcgrath/gamma-tenant-manager:latest -t kevinmcgrath/gamma-tenant-manager:0.1.5
 
-docker build . -t kevinmcgrath/gamma-tenant-manager:0.1.8
+docker build . -t kevinmcgrath/gamma-tenant-manager:0.1.9
 
-docker tag kevinmcgrath/gamma-tenant-manager:0.1.8 kevinmcgrath/gamma-tenant-manager:latest
+docker tag kevinmcgrath/gamma-tenant-manager:0.1.9 kevinmcgrath/gamma-tenant-manager:latest
 
 docker push kevinmcgrath/gamma-tenant-manager:latest
-docker push kevinmcgrath/gamma-tenant-manager:0.1.8
+docker push kevinmcgrath/gamma-tenant-manager:0.1.9
 
 # On the VM:
 # Update ~/docker/configs/config.json if changes were made
